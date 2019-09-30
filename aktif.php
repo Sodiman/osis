990028e82704686746f0510161cfe0c473ad04c5
Ody RC <49949878+Sodiman@users.noreply.github.com>
2019-09-30 23:27:39 +0700

    Update aktif.php

--- aktif.php
+++ aktif.php
@@ -1,15 +1,3 @@
 <?php
-	include "koneksi.php";
-	
-	$cekLogin = $_SESSION['login_user'];
-	$query = mysql_query("select id from tb_user where username='$cekLogin'");
-	$data = mysql_fetch_array($query);
-	if($data['id'] != 1){
-		$bilik = $_GET['bilik'];
-		$jml = $_GET['jumlah'];
-		if ($jml == 0) {
-			mysql_query("insert into tb_hasil (aktif, user) values ('A', '$bilik')");
-		}
-	}
-	header('Location: /osis');
+
 ?>
